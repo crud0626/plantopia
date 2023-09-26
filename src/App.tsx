80bc55309e4ea5b11c0b21ced7b6f11e7e6d9660
[SW5]엄태호 <sisofiy6048@gmail.com>
2023-09-26 20:03:19 +0900

    Refactor: Toast 컴포넌트 리팩토링
    * 컴포넌트명 변경
    
    * 불필요한 props 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useEffect, Suspense } from 'react';
-import { setBodyHeight } from './utils/setBodyHeight';
 import Progress from './components/progress/Progress';
-import Toast from './components/notification/ToastContainer';
+import Toast from './components/toast/Toast';
 import AppRoutes from './routes/AppRoutes';
+import { setBodyHeight } from './utils/setBodyHeight';
+
 import 'react-toastify/dist/ReactToastify.css';
-import '@/styles/customToastStyles.scss';
 import 'react-confirm-alert/src/react-confirm-alert.css';
+import '@/styles/customToastStyles.scss';
 import '@/styles/alertStyle.scss';
 
 const App = () => {
