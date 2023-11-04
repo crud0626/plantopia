2a216d59859a99a8ef6f67d15d4163df7b4ef671
[SW5]엄태호 <sisofiy6048@gmail.com>
2023-11-04 19:44:07 +0900

    Chore: ESLint 설정 변경
    * airbnb, next 추가
    
    * react hooks 플러그인은 airbnb에 포함되어있어 삭제

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,11 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'airbnb',
+    'aribnb/hooks',
+    'airbnb-typescript',
+    'next',
     'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
